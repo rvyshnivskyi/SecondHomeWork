5d99b846dd35b441af910f8bc4b6a486e88712a0
rvyshnivskyi <rvyshnivskyi@playtika.com>
2017-10-05 15:16:54 +0300

    Refactor code and fix method that aggregates word frequencies of all files from start directory

--- src/com/playtika/file/reader/FileReader.java
+++ src/com/playtika/file/reader/FileReader.java
@@ -3,17 +3,17 @@ package com.playtika.file.reader;
 import com.playtika.text.analyzer.Text;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.NotDirectoryException;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.Files.readAllBytes;
 import static java.nio.file.Files.readAttributes;
+import static java.util.Arrays.asList;
 
 public class FileReader {
 	private final File startDirectory;
@@ -28,30 +28,29 @@ public class FileReader {
 		}
 	}
 
-	public static void main(String[] args) {
-		try {
-			FileReader fileReader = new FileReader("resources/text");
-			fileReader.copyFile(new File("text.txt"));
-			Map<File, Map<String, Integer>> wordFrequenciesForFiles = fileReader.getWordFrequenciesForFiles();
-			wordFrequenciesForFiles.keySet().forEach(file -> {
-				try {
-					printFileInformation(file);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			});
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (NotDirectoryException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws IOException {
+		FileReader.copyFile(new File("text.txt"));
+		Map<String, Integer> wordFrequencies = new FileReader("resources/text").getWordFrequenciesForFiles();
+		System.out.println(wordFrequencies);
+	}
+
+	public Map<String, Integer> getWordFrequenciesForFiles() {
+		Map<String, Integer> wordFrequencies = new HashMap<>();
+		List<File> files = getAllIncludedFiles();
+		files.forEach(file -> {
+			try {
+				printFileInformation(file);
+				new Text(getStringFromFile(file)).getWordFrequencies()
+						.forEach((word, frequencies) -> wordFrequencies.merge(word, frequencies, Integer::sum));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+		return wordFrequencies;
 	}
 
 	public static void printFileInformation(File file) throws IOException {
-		BasicFileAttributes fileAttributes = null;
-		fileAttributes = readAttributes(file.toPath(), BasicFileAttributes.class);
+		BasicFileAttributes fileAttributes = readAttributes(file.toPath(), BasicFileAttributes.class);
 		System.out.printf(
 				"File path: %s | size: %d Bytes | creation date: %s%n",
 				file.getAbsolutePath(),
@@ -59,37 +58,26 @@ public class FileReader {
 				new SimpleDateFormat("dd-MM-yyyy HH:mm").format(fileAttributes.creationTime().toMillis()));
 	}
 
-	public Map<File, Map<String, Integer>> getWordFrequenciesForFiles() {
-		Map<File, Map<String, Integer>> filesWordFrequencies = new HashMap<>();
-		getAllFilesTextAsString().forEach((file, fileText) -> filesWordFrequencies.put(file, new Text(fileText).getWordFrequencies()));
-		return filesWordFrequencies;
-	}
-
-	public void copyFile(File file) throws IOException {
+	public static void copyFile(File file) throws IOException {
 		File newFile = new File(file.getPath().replaceAll("(?s)(\\.)(?!.*?(\\.))", "New."));
-		try (InputStream inputStream = new BufferedInputStream(new FileInputStream(file)); OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(newFile))) {
+		if (newFile.exists()) {
+			newFile.delete();
+		}
+		try (InputStream in = new BufferedInputStream(new FileInputStream(file));
+			 OutputStream out = new BufferedOutputStream(new FileOutputStream(newFile))) {
 			byte[] buffer = new byte[1024];
 			int length;
-			while ((length = inputStream.read(buffer)) > 0) {
-				outputStream.write(buffer, 0, length);
+			while ((length = in.read(buffer)) > 0) {
+				out.write(buffer, 0, length);
 			}
 		}
 	}
 
-	private Map<File, String> getAllFilesTextAsString() {
-		Map<File, String> fileStringsMap = new HashMap<>();
-		getAllIncludedFiles().forEach(
-				(file) -> {
-					try {
-						fileStringsMap.put(file, new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8));
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				});
-		return fileStringsMap;
+	private String getStringFromFile(File file) throws IOException {
+		return new String(new String(readAllBytes(file.toPath()), UTF_8));
 	}
 
 	private List<File> getAllIncludedFiles() {
-		return Arrays.asList(startDirectory.listFiles(File::isFile));
+		return asList(startDirectory.listFiles(File::isFile));
 	}
 }
